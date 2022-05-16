28d4f9945aa41b7da4d1986df89b22a0ddc3b2b1
Max Rodriguez <maxrcoppola@gmail.com>
2022-05-16 16:51:59 -0700

    package: Init npm package & license

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/*
+    Released under the MIT license.
+    View 'LICENSE' for the full license.
+
+    Copyright (c) 2022 Max Rodriguez
+*/
+export * from './poparazzi';
---
+++ src/poparazzi.ts
@@ -0,0 +1,9 @@
+/*
+    Released under the MIT license.
+    View 'LICENSE' for the full license.
+
+    Copyright (c) 2022 Max Rodriguez
+*/
+export class PoparazziClient {
+
+}
