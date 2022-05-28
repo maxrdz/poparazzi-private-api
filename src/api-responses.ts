6cf5f9a60dc5416c326ed8ee5863711a2114812f
Max Rodriguez <maxrcoppola@gmail.com>
2022-05-27 22:58:09 -0700

    license: Released under the Apache-2.0 license

--- src/api-responses.ts
+++ src/api-responses.ts
@@ -1,12 +1,17 @@
 /*
-    poparazzi-private-api  Copyright (C) 2022  Max Rodriguez
-    This program comes with ABSOLUTELY NO WARRANTY;
-    This is free software, and you are welcome to
-    redistribute it under certain conditions;
+    Copyright 2022 Max Rodriguez Coppola
 
-    Defines Poparazzi API endpoint responses.
-    Each response object defines the data that
-    is placed inside the 'data' object of an API response.
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
 
 class ApiResponseBase {
